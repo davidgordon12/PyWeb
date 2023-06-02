5199b96a4e4ed5999db9172d43ff23325ac02fda
davidgordon12 <davidgordon12@live.ca>
2023-06-01 21:15:06 -0400

    Print game board

--- main.c
+++ main.c
@@ -3,15 +3,49 @@
 #define BOARD_WIDTH 20
 #define BOARD_HEIGHT 12
 
+#define BORDER = 1
+#define WHITESPACE = 2
+#define SNAKE = 3
+
+void init_board(int board[BOARD_WIDTH][BOARD_HEIGHT]);
+void draw_board(int board[BOARD_WIDTH][BOARD_HEIGHT]);
+
 int main() {
     int board[BOARD_WIDTH][BOARD_HEIGHT];
 
+    init_board(board);
+
+    draw_board(board);
+
+    return -1;
+}
+
+void init_board(int board[BOARD_WIDTH][BOARD_HEIGHT]) {
     for(int x = 0; x < BOARD_WIDTH; x++) {
         for(int y = 0; y < BOARD_HEIGHT; y++) {
-            board[x][y] = 1;
+            if(x == 0 || y == 0) {
+                board[x][y] BORDER;    
+            }
+            else {
+                board[x][y] WHITESPACE;
+            }
         }
     }
+}
 
-    printf("%d \n", board[5][5]);
-    return -1;
+void draw_board(int board[BOARD_WIDTH][BOARD_HEIGHT]) {
+    for(int y = 0; y < BOARD_HEIGHT; y++) {
+        for(int x = 0; x < BOARD_WIDTH; x++) {
+            printf(" ");
+            if(board[x][y] == 1) {
+                printf("#");
+            }
+            else if(board[x][y] == 2) {
+                printf("*");
+            }
+            if(x == 19) {
+                printf("\n");
+            }
+        }
+    }
 }
