eec54cef538af7240537495d1397188caa9923cd
davidgordon12 <davidgordon12@live.ca>
2023-06-08 15:59:04 -0400

    Revert movement code

--- game.c
+++ game.c
@@ -1,6 +1,7 @@
 #include "game.h"
 
 int count = 0;
+int len = 0;
 
 void init_board(int board[BOARD_WIDTH][BOARD_HEIGHT]) {
     for (int x = 0; x < BOARD_WIDTH; x++) {
@@ -21,6 +22,12 @@ void init_board(int board[BOARD_WIDTH][BOARD_HEIGHT]) {
 }
 
 void draw_board(int board[BOARD_WIDTH][BOARD_HEIGHT], snake_t *snake) {
+    snake_t* head = snake;
+    while(head != NULL) {
+        board[head->x_pos][head->y_pos] = SNAKE;
+        head = head->prev;
+    }
+
     for (int y = 0; y < BOARD_HEIGHT; y++) {
         for (int x = 0; x < BOARD_WIDTH; x++) {
             printf(" ");
@@ -62,6 +69,7 @@ void run() {
     snake->x_pos = 10;
     snake->y_pos = 6;
     snake->direction = 'w';
+    snake->prev = NULL;
 
     int board[BOARD_WIDTH][BOARD_HEIGHT];
 
@@ -105,10 +113,7 @@ void run() {
         }
     }
 
-    while (snake != NULL) {
-        free(snake);
-        snake = snake->prev;
-    }
+    printf("\n Game Over! Final score: %d", count);
 }
 
 void place_random_apple(int board[BOARD_WIDTH][BOARD_HEIGHT]) {
@@ -125,26 +130,50 @@ void place_random_apple(int board[BOARD_WIDTH][BOARD_HEIGHT]) {
     board[rand_x][rand_y] = APPLE;
 }
 
-int move_snake(int board[BOARD_WIDTH][BOARD_HEIGHT], snake_t *snake) {
-    while (snake != NULL) {
-        switch (snake->direction) {
-            case UP:
-                snake->y_pos--;
-                break;
-            case DOWN:
-                snake->y_pos++;
-                break;
-            case LEFT:
-                snake->x_pos--;
-                break;
-            case RIGHT:
-                snake->x_pos++;
-                break;
-        }
+int move_snake(int board[BOARD_WIDTH][BOARD_HEIGHT], snake_t* snake) {
+    int prev_x = snake->x_pos;
+    int prev_y = snake->y_pos;
+    board[snake->x_pos][snake->y_pos] = WHITESPACE;
 
-        board[snake->x_pos][snake->y_pos] = SNAKE;
-        snake = snake->prev;
+    if(snake->direction == UP) {
+        snake->y_pos--;
     }
+
+    if(snake->direction == DOWN) {
+        snake->y_pos++;
+    }
+    
+    if(snake->direction == LEFT) {
+        snake->x_pos--;
+    }
+
+    if(snake->direction == RIGHT) {
+        snake->x_pos++;
+    }
+
+    if(board[snake->x_pos][snake->y_pos] == APPLE) {
+        count++;
+        place_random_apple(board);
+        board[prev_x][prev_y] = SNAKE;
+
+        snake_t* new_head = (snake_t*)malloc(sizeof(snake_t));
+
+        new_head->direction = snake->direction;
+        new_head->x_pos = snake->x_pos;
+        new_head->y_pos = snake->y_pos;
+
+        new_head->prev = snake;
+        snake = new_head;
+    }
+
+    /* 
+     *  while(head)
+     *    old_x = head->x, old_y = head->y
+     *    head = head.next
+     *    head->x = old_x, head->y = old_y
+     */
+
+    board[snake->x_pos][snake->y_pos] = SNAKE;
 
     return 1;
 }
